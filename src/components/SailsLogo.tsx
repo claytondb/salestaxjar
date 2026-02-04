eae225427af07340ee35ad14640fa25a2a0981ce
root <root@DAVIDPC.>
2026-02-04 15:26:56 -0600

    Replace SVG logo with new sailboat logo

--- src/components/SailsLogo.tsx
+++ src/components/SailsLogo.tsx
@@ -1,16 +1,14 @@
+import Image from 'next/image';
+
 export default function SailsLogo({ className = "w-10 h-10" }: { className?: string }) {
   return (
-    <svg 
-      viewBox="0 0 100 100" 
+    <Image
+      src="/logo.jpg"
+      alt="Sails logo"
+      width={101}
+      height={101}
       className={className}
-      fill="currentColor"
-    >
-      {/* Main sail */}
-      <path d="M50 8 L50 75 L15 75 Z" />
-      {/* Small sail */}
-      <path d="M50 20 L50 60 L70 60 Z" />
-      {/* Hull */}
-      <path d="M10 78 Q15 90 50 90 Q85 90 90 78 L10 78 Z" />
-    </svg>
+      style={{ objectFit: 'contain' }}
+    />
   );
 }
